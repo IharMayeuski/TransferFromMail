67eebdd2931013b68fc3d413942d8080e6f776d1
Maevskiy <maevskij.i@gmail.com>
2020-02-11 00:14:19 +0300

    MIV-001

---
+++ src/main/java/by/transfer/maevo/Main.java
@@ -0,0 +1,53 @@
+package by.transfer.maevo;
+
+
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+import java.util.List;
+
+import static by.transfer.maevo.Constant.*;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        ServiceFillScene fillSceneService = new ServiceFillScene();
+        Service service = new Service();
+        List<String> elements = service.getEmailList();
+
+        ObservableList<String> langs = FXCollections.observableArrayList(elements);
+        ComboBox<String> langsComboBox = new ComboBox<>(langs);
+        langsComboBox.setValue(elements.get(0));
+
+        GridPane grid = new GridPane();
+        Scene scene = new Scene(grid, 200, 160);
+        Button button = new Button(BUTTON_NAME);
+
+        grid.add(new FlowPane(150, 150, langsComboBox), 0, 2);
+        fillSceneService.fillScene(grid, SCENE_NAME);
+        fillSceneService.fillGrid(grid);
+        fillSceneService.createButton(grid, button, 3);
+
+        button.setOnAction(event ->
+                System.out.println(langsComboBox.getValue()
+
+                ));
+        stage.setTitle(TITLE_NAME);
+        stage.setScene(scene);
+        stage.show();
+
+
+    }
+}
---
+++ src/main/java/by/transfer/maevo/ServiceFillScene.java
@@ -0,0 +1,32 @@
+package by.transfer.maevo;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+
+class ServiceFillScene {
+    void fillGrid(GridPane grid) {
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+    }
+
+    void fillScene(GridPane grid, String sceneName) {
+        Text scenetitle = new Text(sceneName);
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(scenetitle, 0, 0, 2, 1);
+    }
+
+    void createButton(GridPane grid, Button button, int row) {
+        HBox hBox = new HBox(40);
+        hBox.setAlignment(Pos.BOTTOM_LEFT);
+        hBox.getChildren().add(button);
+        grid.add(hBox, 0, row);
+    }
+}
